d2e3ce08e6079bd078d73404b4dc7ee8bf0ed35e
123samson456 <breitung.programming@gmx.de>
2016-06-27 11:51:30 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -122,7 +122,52 @@ function state_pause:draw()
 	
 end
 
+function state_gameover:enter()
+	gameover 	= 	{}
+	gameover.res	= 	require( "resources.gameover" )
+end
+
+function state_gameover:update( dt )
+	
+end
+
+function state_gameover:draw()
+	
+end
+
 function love.load()
 	Gamestate.registerEvents()
 	Gamestate.switch( state_mainmenu )
+end
+
+function love.update( dt )
+	
+end
+
+function love.draw()
+	
+end
+
+function love.keypressed( key, code )
+	
+end
+
+function love.keyreleased( key, code )
+	
+end
+
+function love.mousepressed( x, y, button )
+	
+end
+
+function love.mousereleased( x, y, button )
+	
+end
+
+function love.focus( f )
+	
+end
+
+function love.quit()
+	
 end
