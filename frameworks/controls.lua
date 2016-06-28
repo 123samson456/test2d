a9d5247fdcac4a639b5768a5d7762ba336baede0
123samson456 <breitung.programming@gmx.de>
2016-06-28 11:15:12 +0200

    Update controls.lua

--- frameworks/controls.lua
+++ frameworks/controls.lua
@@ -6,6 +6,7 @@ local path_to_controls  =   "settings/controls.txt"
 
 for line in love.filesystem.lines( path_to_controls ) do
  	for action, key, expl in line:gmatch( "(.*)\t(.*)\t(.*)" ) do
+ 	 if tonumber( key ) ~= nil then key = tonumber( key ) end
  		controls[ action ] 	= 	key
  		controlStrings[ action ] 	= 	expl
  	end
