b01fa0a174430ac02817561db6a86156b516380d
123samson456 <breitung.programming@gmx.de>
2016-06-30 13:05:40 +0200

    Update mainmenu.lua

--- resources/mainmenu.lua
+++ resources/mainmenu.lua
@@ -1,5 +1,31 @@
 local res   =   {}
-res.buttons   =   {}
 res.cursor    =       -- path to custom cursor
+
+local button1 	= 	gui.Create( "button" )
+button1:SetState( "state_mainmenu" )
+button1:SetSize( 100, 100 )
+button1:SetText( "Play" )
+
+local button2 	= 	gui.Create( "button" )
+button2:SetState( "state_mainmenu" )
+button2:SetSize( 100, 100 )
+button2:SetText( "Settings" )
+
+local button3	= 	gui.Create( "button" )
+button3:SetState( "state_mainmenu" )
+button3:SetSize( 100, 100 )
+button3:SetText( "Credits" )
+
+local button4 =   gui.Create( "button" )
+button4:SetState( "state_mainmenu" )
+button4:SetSize( 100, 100 )
+button4:SetText( "Quit" )
+
+res.buttons   =   {
+	{ obj = button1, target = "" },
+	{ obj = button2, target = "" },
+	{ obj = button3, target = "" },
+	{ obj = button4, target = "" }
+}
 
 return res
