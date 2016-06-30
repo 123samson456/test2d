c694045e808f8821f16be47fbe73b02e2506de3c
123samson456 <breitung.programming@gmx.de>
2016-06-30 15:49:39 +0200

    Update controls.lua

--- frameworks/controls.lua
+++ frameworks/controls.lua
@@ -1,15 +1,30 @@
 -- Initializing Controls
 
-local controls  =   {}
-local controlStrings	= 	{}
-local path_to_controls  =   "settings/controls.txt"
+local input  =  require( "frameworks.input" )()
+local explanation	= 	{}
+local path_to_controls  =  "settings/controls.txt"
 
-for line in love.filesystem.lines( path_to_controls ) do
- 	for action, key, expl in line:gmatch( "(.*)\t(.*)\t(.*)" ) do
- 	 if tonumber( key ) ~= nil then key = tonumber( key ) end
- 		controls[ action ] 	= 	key
- 		controlStrings[ action ] 	= 	expl
- 	end
+for line in love.filesystem.load( path_to_controls ) do
+	for action, key, expl in line:gmatch( "(.+)\t(.*)\t(.*)" ) do
+		input:bind( key, action )
+		explanation[ action ]	= 	expl
+	end
 end
 
-return controls, controlStrings
+return explanation
+
+--[[
+		If you want to check whether an input is being used or not use this:
+		
+		function love.update(dt)
+			if input:pressed('print') then print(1) end
+			if input:released('print') then print(2) end
+			if input:down('left_click') then print('left click down') end
+		end
+		
+		To completely unbind an action / key use this:
+		
+		input:unbind('1')
+		input:unbind('s')
+		input:unbind('mouse1')
+]]--
